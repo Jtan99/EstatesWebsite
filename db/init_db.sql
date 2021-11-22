c05e812657f706afbc229abb695e0e757dc0543f
rsg23 <rajan_grewal_2@sfu.ca>
2021-11-21 22:38:44 -0800

    new table + enhancements idk if it works yet

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE User(
+CREATE TABLE user(
 	`uid` SMALLINT(6) NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(60),
 	`email` VARCHAR(60),
@@ -8,8 +8,17 @@ CREATE TABLE User(
 	PRIMARY KEY (`uid`)
 );
 
-INSERT INTO User(first_name, last_name, email, phone_number, notes) VALUES("user1", "user1@hotmail.com", "user1", "user1 test" "123-456-7890");
-INSERT INTO User(first_name, last_name, email, phone_number, notes) VALUES("user2", "user2@hotmail.com", "user2", "user2 test", "987-654-3210");
+CREATE TABLE listing(
+	`listingid` SMALLINT(6) NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(200) NOT NULL,
+	`price` INT NOT NULL,
+	`listing_type` VARCHAR(45) NOT NULL,
+	`description` VARCHAR(2000),
+	PRIMARY KEY (`listingid`)
+);
+
+INSERT INTO user(uid, username, email, password, full_name, phone_number) VALUES(1, "user1", "user1@hotmail.com", "pw1", "user1 test" "123-456-7890");
+INSERT INTO user(uid, username, email, password, full_name, phone_number) VALUES(2, "user2", "user2@hotmail.com", "pw2", , "user2 test", "987-654-3210");
 
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
 
