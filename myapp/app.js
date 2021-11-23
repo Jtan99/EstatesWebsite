1f52f8e5d89043117e098dc8d2c49ecc010817f6
Johnson Tan <jta118@sfu.ca>
2021-11-23 02:22:19 -0800

    Moved code from index router to users router. Added express-session

--- myapp/app.js
+++ myapp/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var sessionRouter = require('./routes/session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -28,8 +29,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(sessionRouter)
+app.use('/index', indexRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
