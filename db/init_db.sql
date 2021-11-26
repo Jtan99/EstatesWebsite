02390431716c85dba0aaacf4de356e6e698a2230
rsg23 <rajan_grewal_2@sfu.ca>
2021-11-25 23:29:01 -0800

    added more tables for listing info

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,25 +1,58 @@
-CREATE TABLE user(
-	`uid` SMALLINT(6) NOT NULL AUTO_INCREMENT,
-	`username` VARCHAR(60),
-	`email` VARCHAR(60),
-	`password` VARCHAR(60),
-	`full_name` VARCHAR(120),
-	`phone_number` VARCHAR(60),
-	PRIMARY KEY (`uid`)
+CREATE TABLE Users(
+	`userid` SMALLINT(6) NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(60) NOT NULL,
+	`email` VARCHAR(60) NOT NULL,
+	`password` VARCHAR(60) NOT NULL,
+	`full_name` VARCHAR(60) NOT NULL,
+	`role` VARCHAR(60) NOT NULL,
+	PRIMARY KEY (`userid`)
+);
+
+
+CREATE TABLE building(
+  `buildingid` SMALLINT(6) NOT NULL AUTO_INCREMENT,
+  `bathrooms` INT NOT NULL,
+  `bedrooms` INT NOT NULL,
+  `floor_space` INT NULL,
+  `building_type` VARCHAR(60) NULL,
+  `storeys` INT NULL,
+  `appliances` VARCHAR(200) NULL,
+  PRIMARY KEY (`buildingid`)
+);
+
+CREATE TABLE property(
+  `propertyid` SMALLINT(6) NOT NULL AUTO_INCREMENT,
+  `property_age` INT NULL,
+  `annual_property_tax` DECIMAL(9,2) NULL,
+  `parking_type` VARCHAR(60) NULL,
+  `amenities` VARCHAR(200) NULL,
+  PRIMARY KEY (`propertyid`)
+);
+
+CREATE TABLE location(
+  `locationid` SMALLINT(6) NOT NULL AUTO_INCREMENT,
+  `country` VARCHAR(60) NOT NULL,
+  `province_state` VARCHAR(60) NOT NULL,
+  `address` VARCHAR(60) NOT NULL,
+  `postal_code` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`locationid`)
 );
 
 CREATE TABLE listing(
 	`listingid` SMALLINT(6) NOT NULL AUTO_INCREMENT,
+	`buildingid` SMALLINT(6) NOT NULL DEFAULT 1,
+	`propertyid` SMALLINT(6) NOT NULL DEFAULT 1,
+	`locationid` SMALLINT(6) NOT NULL DEFAULT 1,
 	`title` VARCHAR(200) NOT NULL,
 	`price` INT NOT NULL,
-	`listing_type` VARCHAR(45) NOT NULL,
-	`description` VARCHAR(2000),
-	PRIMARY KEY (`listingid`)
+	`listing_type` VARCHAR(60) NOT NULL,
+	`description` VARCHAR(2000) NULL,
+	PRIMARY KEY (`listingid`),
+	FOREIGN KEY (`buildingid`) REFERENCES building(`buildingid`),
+	FOREIGN KEY (`propertyid`) REFERENCES property(`propertyid`),
+	FOREIGN KEY (`locationid`) REFERENCES location(`locationid`)
 );
 
-INSERT INTO user(uid, username, email, password, full_name, phone_number) VALUES(1, "user1", "user1@hotmail.com", "pw1", "user1 test" "123-456-7890");
-INSERT INTO user(uid, username, email, password, full_name, phone_number) VALUES(2, "user2", "user2@hotmail.com", "pw2", , "user2 test", "987-654-3210");
+INSERT INTO Users(username, email, password, full_name, role) VALUES("admin", "admin@hotmail.com", "admin", "admin user", "admin");
 
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
-
-flush privileges;
