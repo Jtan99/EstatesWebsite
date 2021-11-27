c7e1785a1a3add090bd1242755e60e2e52a78760
Eric Leung <ericl.leung@gmail.com>
2021-11-26 18:28:31 -0800

    google maps and geocoding for listing locations

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test-map', (req, res, next) => {
+  res.render('map2', {API_KEY: 'AIzaSyAytC_TusuhG7kpNQ19hMrCzXDIUjd307o', address: 'sfu burnaby' });
+})
+
+
 module.exports = router;
