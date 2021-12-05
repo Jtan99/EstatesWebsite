ad67b56382ca11d2b6f9b04fcf71f5412224d983
brandon <bbha@sfu.ca>
2021-12-04 22:03:06 -0800

    removed mysql comment

--- myapp/app.js
+++ myapp/app.js
@@ -12,15 +12,6 @@ var addNewListingRouter = require('./routes/add-new-listing');
 var viewProfileRouter = require('./routes/profile');
 var viewHomePageRouter = require('./routes/home');
 
-// var mysql = require('mysql');
-// var connection = mysql.createPool({
-//   connectionLimit: 10,
-//   host: process.env.MYSQL_HOST || 'localhost',
-//   user: process.env.MYSQL_USER || 'root',
-//   password: process.env.MYSQL_PASSWORD || 'password',
-//   database: process.env.MYSQL_DATABASE || 'mydb'
-// });
-
 var app = express();
 
 // view engine setup
