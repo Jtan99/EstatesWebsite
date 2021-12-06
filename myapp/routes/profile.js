0d603d9b21996dd83cc3f5d5ef5f04c13f90466d
brandon <bbha@sfu.ca>
2021-12-06 04:05:33 -0800

    passed values to profile.ejs for rendering

--- myapp/routes/profile.js
+++ myapp/routes/profile.js
@@ -5,13 +5,22 @@ var db = require('/myapp/routes/connection');
 
 /* GET user Profile. */
 router.get('/', currSession.checkSessionStatus, (req, res) => {
-  res.render('profile');
-
-  db.connection.query('select * from user', (err, results) => {
+  // var user_name = req.session.user[0]["username"];
+  // console.log("this is username: " + user_name);
+  var query = `SELECT * FROM user WHERE username=?`;
+  var input = [req.session.user[0]["username"]];
+  db.connection.query(query, input, (err, results) => {
     if (err) console.log('ERRN:', err.message)
     else 
-    console.log('res', results)
+    // console.log('res', results);
+    // console.log('info', results[0].username);
+    var user_info = results[0];
+    res.render('profile', {username: user_info.username, email: user_info.email, password: user_info.password, full_name: user_info.full_name, role: user_info.role});
+  
   });
 });
+
+
+
 
 module.exports = router;
