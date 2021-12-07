c9445b17bd2d14e4692335337da10037370d0b28
brandon <bbha@sfu.ca>
2021-12-06 17:45:35 -0800

    added navbar and session checker to new listing

--- myapp/routes/new-listing.js
+++ myapp/routes/new-listing.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var currSession = require('/myapp/routes/session');
+var db = require('/myapp/routes/connection');
 
 /* GET new listing page. */
-router.get('/', function(req, res, next) {
-    res.render('new-listing', {title: 'New Listing'});
+router.get('/', currSession.checkSessionStatus, function(req, res, next) {
+    var query = `SELECT * FROM user WHERE username=?`;
+    var input = [req.session.user[0]["username"]];
+    db.connection.query(query, input, (err, rows) => {
+      if(err){
+        res.json({
+          success: false,
+          err
+          });
+      }
+      else{
+        var user_info = rows[0];
+        res.render('new-listing', {full_name: user_info.full_name});
+      }
+    });
+    
+    
 });
 module.exports = router;
