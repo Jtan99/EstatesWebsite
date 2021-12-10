da8a99f5a9857b19614d074df8ecc182d5e55354
Eric Leung <ericl.leung@gmail.com>
2021-12-09 17:40:20 -0800

    basic display listing page

--- myapp/routes/view-listing.js
+++ myapp/routes/view-listing.js
@@ -3,14 +3,15 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/:id", function (req, res, next) {
-	// get from db
+	// get from db:
 	connection.query(
 		`SELECT *
-        FROM listing
-        INNER JOIN building ON listing.buildingid=building.buildingid
-        INNER JOIN property ON listing.propertyid=property.propertyid
-        INNER JOIN location ON listing.locationid=location.locationid
-        WHERE listingid= ?`,
+		FROM (SELECT *
+			  FROM listing
+			  WHERE listingid=?) AS listing
+		INNER JOIN building ON listing.buildingid=building.buildingid
+		INNER JOIN property ON listing.propertyid=property.propertyid
+		INNER JOIN location ON listing.locationid=location.locationid`,
 		[req.params.id],
 		(err, results) => {
 			if (err) {
@@ -18,10 +19,36 @@ router.get("/:id", function (req, res, next) {
 			} else if (!results.length) {
 				res.status(404).send({ message: "Not Found" });
 			} else {
-                res.render("view-home-listing", { listing: results[0] });
+				console.log("Results", results[0])
+                res.render("view-home-listing", { listing: results[0], API_KEY: "AIzaSyAytC_TusuhG7kpNQ19hMrCzXDIUjd307o" });
 			}
 		}
     );
+	// let listing = {
+	// 	listingid: 1,
+	// 	buildingid: 1,
+	// 	propertyid: 1,
+	// 	locationid: 1,
+	// 	title: 'House1',
+	// 	price: 13223,
+	// 	listing_type: 'rental',
+	// 	description: 'huge mansion',
+	// 	bathrooms: 312,
+	// 	bedrooms: 132,
+	// 	floor_space: 31222,
+	// 	building_type: 'mansion',
+	// 	storeys: 132,
+	// 	appliances: 'everything',
+	// 	property_age: 231,
+	// 	annual_property_tax: 312332,
+	// 	parking_type: '12 car garage',
+	// 	amenities: 'all',
+	// 	country: 'canada',
+	// 	province_state: 'bc',
+	// 	address: 'sfu burnaby',
+	// 	postal_code: 'v82jsd2'
+	//   }
+	//   res.render("view-home-listing", { listing: listing });
 });
 
 module.exports = router;
